64e17e5cb0b88feb02107e7b1ed8d4e5a4619b0b
l4-ky <82997614+l4-ky@users.noreply.github.com>
2024-11-06 14:40:57 +0100

    TestClient update

--- Progetto-IRC/src/TestClient.java
+++ Progetto-IRC/src/TestClient.java
@@ -10,13 +10,17 @@ public class TestClient {
 			System.out.print("Inserisci un nuovo messaggio da inviare al server ('/quit' per terminare): ");
 			input = scanner.nextLine();
 			if(input.equals("/?")) {
-				System.out.println("/quit : Disconnect;\n/whisper : Send a direct message;\n/nick : Request to change your nickname;\n/info : Request information of the given type.\n(all the commands above are valid in the current channel's domain.)");
+				System.out.println("/info : Request information of the given type;\n/whisper : Send a direct message;\n/nick : Request to change your nickname;\n/mute : Prevent someone else to send messages;\n/quit : Disconnect.\n(all the commands above are valid in the current channel's domain.)");
 			} else if (input.equals("/info ?")) {
 				System.out.println("Action: request information of the given type.\nSyntax: /info infoType\nTypes: all, partList, ...");			
 			} else if (input.equals("/whisper ?")) {
 				System.out.println("Action: send a direct message\nSyntax: /whisper recipientName\n(recipientName has to be a valid client name)");	
 			} else if (input.equals("/nick ?")) {
 				System.out.println("Action: request to change your nickname\nSyntax: /nick newNickname\n(newNickname has to be a valid nickname)");
+			} else if(input.equals("/mute ?")) {
+				System.out.println("Action: deny another user to send any kind of messages for a given span of time\nSyntax: /mute targetName timeSpan(seconds)");
+			} else if(input.equals("/quit ?")) {
+				System.out.println("Action: disconnect from the channel\nSyntax: /quit\n(this action cannot be undone)");
 			} else if(input.equals("/quit")) {
 				client.invia(new Pacchetto("",410));
 			} else if(input.startsWith("/whisper")) {
@@ -28,6 +32,13 @@ public class TestClient {
 			} else if(input.startsWith("/info")) {
 				String[] segments = input.split(" ",2);
 				client.retrieveInfo(segments[1]);
+			} else if(input.startsWith("/mute")) {
+				String[] segments = input.split(" ",3);
+				try {
+					client.mute(segments[1], Integer.parseInt(segments[2]));
+				} catch(NumberFormatException ex) {
+					System.out.println("Wrong type input (targetName must be Sting ; timeSpan must be integer).");
+				}
 			} else {
 				client.invia(new Pacchetto(input,200));
 			}
